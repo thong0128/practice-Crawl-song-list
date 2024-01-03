d41cfbd535456117100a5e70e57a14199e832022
thong0128 <thong0128@gmail.com>
2024-01-03 15:20:23 +0700

    unDone

---
+++ src/CrawlSongExample.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlSongExample {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://soundcloud.com/nghe-nh-c/sets/nh-ng-b-i-h-t-hay-nh-t-c-a-chi");
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+
+            scanner.close();
+            content = content.replaceAll("\\n+", "");
+
+            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
+            Matcher m = p.matcher(content);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
